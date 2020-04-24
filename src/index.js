e23ec81ea3e1930facfd52cd58ca797a00f3a085
Theouty <aman.david.michael@gmail.com>
2020-04-24 08:12:02 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const taskList = document.getElementById("list");
+  const form = document.getElementById('create-task-form');
+  
+
+  form.addEventListener('submit', function(e){
+    e.preventDefault()
+    let li = document.createElement("li")
+    li.textContent = e.target["new-task-description"].value;
+
+    let buttonDelete = document.createElement('button');
+    buttonDelete.textContent = "delete"
+    li.appendChild(buttonDelete);
+
+    buttonDelete.addEventListener('click', function(e){
+      e.preventDefault()
+      buttonDelete.parentElement.remove()
+    })        
+    
+    taskList.appendChild(li);
+  })
 });
